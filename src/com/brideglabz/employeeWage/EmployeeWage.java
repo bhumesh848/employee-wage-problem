f46cba85839c613e5f059cf05c426ef26f28314d
Mac02 <mac02@Mac02s-MacBook-Pro.local>
2025-01-10 14:58:02 +0530

    Uc3 for part time employee wage

--- src/com/brideglabz/employeeWage/EmployeeWage.java
+++ src/com/brideglabz/employeeWage/EmployeeWage.java
@@ -3,6 +3,7 @@ package com.brideglabz.employeeWage;
 public class EmployeeWage {
     public static final int WAGE_PER_HOUR = 20;
     public static final int FULL_DAY_HOUR = 8;
+    public static final int PART_TIME_HOUR = 8;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation program");
@@ -19,5 +20,9 @@ public class EmployeeWage {
 
         int dailyEmpWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
         System.out.println("Daily Employee Wage is "+ dailyEmpWage);
+
+        int partTimeWagePerhour = 15;
+        int partTimeWage = PART_TIME_HOUR * partTimeWagePerhour;
+        System.out.println("Part time employe wage is " + partTimeWage);
     }
 }
