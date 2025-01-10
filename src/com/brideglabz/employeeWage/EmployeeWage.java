3228250996445b59d09650cc2e5b5064c369b911
Mac02 <mac02@Mac02s-MacBook-Pro.local>
2025-01-10 14:35:09 +0530

    uc1 to check employee present or absent

--- src/com/brideglabz/employeeWage/EmployeeWage.java
+++ src/com/brideglabz/employeeWage/EmployeeWage.java
@@ -3,5 +3,15 @@ package com.brideglabz.employeeWage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation program");
+        double randomNumber = Math.random() * 2;
+        System.out.println("random number generated "+ randomNumber);
+        int result = (int) randomNumber;
+        System.out.println("result : "+ result);
+        if (result == 1){
+            System.out.println("Employee is present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }
 }
