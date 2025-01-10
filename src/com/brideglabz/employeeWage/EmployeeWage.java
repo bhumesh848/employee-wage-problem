a6da5da580f73e615bbc1f52b85c06464058a819
Mac02 <mac02@Mac02s-MacBook-Pro.local>
2025-01-10 12:21:12 +0530

    Displayed welcome to employee wage program on master branch

---
+++ src/com/brideglabz/employeeWage/EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.brideglabz.employeeWage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation program");
+    }
+}
